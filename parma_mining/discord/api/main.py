a5650be315381872e5990ed1e0249debb0bac9b1
Ege Kocabas <kocabas.ege@tum.de>
2024-01-08 23:32:19 +0100

    refactor: Support different log levels for different environments

--- parma_mining/discord/api/main.py
+++ parma_mining/discord/api/main.py
@@ -13,7 +13,15 @@ from parma_mining.discord.model import (
     ServersRequest,
 )
 
-logging.basicConfig(level=logging.INFO)
+env = os.getenv("env", "local")
+
+if env == "prod":
+    logging.basicConfig(level=logging.INFO)
+elif env in ["staging", "local"]:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.warning(f"Unknown environment '{env}'. Defaulting to INFO level.")
+    logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
